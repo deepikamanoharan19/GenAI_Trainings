2ff9ec3e12878011c346adad5660c043615fbc5d
deepikamanoharan19 <146546030+deepikamanoharan19@users.noreply.github.com>
2025-11-11 11:49:39 +0530

    Python_API_Demo.py

--- Python_API_Demo.py
+++ Python_API_Demo.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key='AIzaSyCJ9H_KCORPVgJ27e44qbzKwmCQmtC5InQ')
+client = genai.Client(api_key='GEMINI_API_KEY')
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
